a0041052be5f135c4b9b2444fb3772e492c011c4
Alex Bostock <alex@scribelabs.ai>
2023-08-30 09:58:45 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'unicorn', 'sonarjs', 'jest', 'promise'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+    'plugin:unicorn/all',
+    'plugin:sonarjs/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:promise/recommended',
+  ],
+  rules: {
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-keyword-prefix': 'off',
+    'unicorn/catch-error-name': ['error', { name: 'err' }],
+    'unicorn/prefer-ternary': ['error', 'only-single-line'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true, argsIgnorePattern: '^_' },
+    ],
+    'unicorn/no-new-array': 'off',
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/import-index': ['error', { ignoreImports: true }],
+    'unicorn/filename-case': ['error', { case: 'camelCase' }],
+    'sonarjs/no-duplicate-string': ['error', { threshold: 5 }],
+  },
+  parserOptions: {
+    project: ['./tsconfig.json', './tests/tsconfig.json'],
+  },
+};
---
+++ vitest.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+    },
+    alias: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    testTimeout: 100_000
+  },
+});
